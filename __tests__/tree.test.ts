e34c25023cccb89eded772b3053d57542b34ff60
Brian J Brennan <brianloveswords@gmail.com>
2017-09-15 02:55:49 -0400

    Tree structure pretty much done

--- __tests__/tree.test.ts
+++ __tests__/tree.test.ts
@@ -1,23 +1,121 @@
-import { createEmptyTree, insert, ROOT, Tree } from "../src/tree";
+import {
+    Branch,
+    Leaf,
+    ROOT,
+    Tree,
+    createEmptyTree,
+    find,
+    insert,
+    insertLeaf,
+    calculatePath,
+} from "../src/tree";
+
 import arrayEqual = require("array-equal");
 
+type StringBranch = Branch<string, string>;
+type StringLeaf = Leaf<string, string>;
+type StringTree = Tree<string, string>;
+
 it("createEmptyTree: creates an empty tree", () => {
-    const t: Tree<string, string> = createEmptyTree();
+    const t: StringTree = createEmptyTree();
     expect(arrayEqual(t, [])).toBe(true);
 });
 
-it("insert: inserts a branch into the tree at the root", () => {
-    const t: Tree<string, string> = createEmptyTree();
+it("insert: inserts a branches into the tree", () => {
+    const t: StringTree = createEmptyTree();
 
     const hiBranch = insert(t, {
         key: "hi",
         children: createEmptyTree(),
-    });
+    }) as StringBranch;
 
     const byeLeaf = insert(hiBranch, {
         key: "bye",
         value: "hello goodbye",
     });
 
-    console.dir(byeLeaf);
+    expect(byeLeaf.parent).toBe(hiBranch);
+});
+
+describe("find", () => {
+    let tree: StringTree;
+    beforeEach(() => {
+        tree = createEmptyTree();
+
+        const controlBranch = insert(tree, {
+            key: "Control",
+            children: createEmptyTree(),
+        }) as StringBranch;
+
+        const controlABranch = insert(controlBranch, {
+            key: "a",
+            children: createEmptyTree(),
+        }) as StringBranch;
+
+        const controlABValue = insert(controlABranch, {
+            key: "b",
+            value: "Control a b",
+        });
+
+        const optionBranch = insert(tree, {
+            key: "Option",
+            children: createEmptyTree(),
+        }) as StringBranch;
+
+        const optionAValue = insert(optionBranch, {
+            key: "a",
+            value: "Option a",
+        });
+
+        const optionBValue = insert(optionBranch, {
+            key: "b",
+            value: "Option b",
+        });
+    });
+
+    it("does not find keys that do not exist", () => {
+        const notFound = find(tree, ["nahhh"]);
+        expect(notFound).toBeNull();
+    });
+
+    it("finds surface keys that exist", () => {
+        const found = find(tree, ["Control"]);
+        expect(found).toBeDefined();
+    });
+
+    it("finds deep keys that exist", () => {
+        const found = find(tree, ["Control", "a", "b"]);
+        expect(found).toBeDefined();
+        expect((found as StringLeaf).value).toBe("Control a b");
+    });
+});
+
+describe("insertLeaf", () => {
+    let tree: StringTree;
+    let controlBranch;
+    beforeEach(() => {
+        tree = createEmptyTree();
+
+        controlBranch = insert(tree, {
+            key: "Control",
+            children: createEmptyTree(),
+        }) as StringBranch;
+
+        insert(controlBranch, {
+            key: "a",
+            value: "Control a",
+        });
+    });
+
+    it("inserts into an existing branch", () => {
+        const leaf = insertLeaf(tree, ["Control", "b"], "Control b");
+        expect(leaf.parent).toBe(controlBranch);
+    });
+
+    it.only("make new branches if they don't exist", () => {
+        const insertPath = ["Option", "a", "c"];
+        const leaf = insertLeaf(tree, insertPath, "Option a c");
+        const foundPath = calculatePath(leaf);
+        expect(arrayEqual(foundPath, insertPath)).toBe(true);
+    });
 });
--- src/tree.ts
+++ src/tree.ts
@@ -1,29 +1,31 @@
 interface PartialBranch<K, V> {
     key: K;
-    parent?: TreeNode<K, V>;
+    parent?: Maybe<TreeNode<K, V>>;
     children: Tree<K, V>;
 }
 
 interface PartialLeaf<K, V> {
     key: K;
-    parent?: TreeNode<K, V>;
+    parent?: Maybe<TreeNode<K, V>>;
     value: V;
 }
 
-interface Branch<K, V> {
+export interface Branch<K, V> {
     key: K;
-    parent: TreeNode<K, V>;
+    parent: Maybe<TreeNode<K, V>>;
     children: Tree<K, V>;
 }
 
-interface Leaf<K, V> {
+export interface Leaf<K, V> {
     key: K;
-    parent: TreeNode<K, V>;
+    parent: Maybe<TreeNode<K, V>>;
     value: V;
 }
 
-type TreeNode<K, V> = Leaf<K, V> | Branch<K, V> | Symbol;
+type Maybe<T> = T | null;
+type TreeNode<K, V> = Leaf<K, V> | Branch<K, V>;
 type PartialTreeNode<K, V> = PartialLeaf<K, V> | PartialBranch<K, V>;
+type Path<K> = K[];
 
 export type Tree<K, V> = Array<TreeNode<K, V>>;
 
@@ -33,27 +35,139 @@ export function createEmptyTree<K, V>(): Tree<K, V> {
     return [];
 }
 
-export function insert<K, V, I extends PartialTreeNode<K, V>>(
+export function insert<K, V>(
     tree: Tree<K, V> | Branch<K, V>,
-    item: I,
-): TreeNode<K, V> {
-    let completeItem;
-    if (!Array.isArray(tree)) {
-        completeItem = setParent<K, V>(item, tree);
+    node: PartialTreeNode<K, V>,
+): Branch<K, V> | Leaf<K, V> {
+    let completeNode;
+    if (isBranch(tree)) {
+        completeNode = setParent<K, V>(node, tree);
         tree = tree.children;
     } else {
-        completeItem = setParent<K, V>(item, ROOT);
+        completeNode = setParent<K, V>(node, null);
     }
-    tree.push(completeItem);
-    return completeItem;
+
+    tree.push(completeNode);
+
+    return isBranch(completeNode)
+        ? completeNode as Branch<K, V>
+        : completeNode as Leaf<K, V>;
 }
 
-export function findNode<K, V>(where: K[]): TreeNode<K, V> | null {}
+export function find<K, V>(
+    tree: Tree<K, V> | Branch<K, V>,
+    path: Path<K>,
+): Maybe<TreeNode<K, V>> {
+    if (isBranch(tree)) {
+        tree = tree.children;
+    }
+
+    let currentTree = tree;
+    let count = 0;
+
+    outer: for (const key of path) {
+        count += 1;
+
+        for (const node of currentTree) {
+            if (node.key === key) {
+                if (count === path.length) {
+                    return node;
+                }
+
+                if (isLeaf(node)) {
+                    // node is not a child but we haven't hit the end
+                    // of the search parameters. technically we
+                    // haven't found it, so let's return null.
+                    return null;
+                }
+
+                currentTree = node.children;
+
+                continue outer;
+            }
+        }
+        return null;
+    }
+    return null;
+}
+
+export function insertLeaf<K, V>(
+    tree: Tree<K, V>,
+    path: Path<K>,
+    value: V,
+): Leaf<K, V> {
+    const rootPath = path.slice(0, -1);
+    const leafKey = path[path.length - 1];
+    const branch = find(tree, rootPath);
+
+    if (branch) {
+        if (isLeaf(branch)) {
+            throw new TypeError("Cannot insert into a leaf node");
+        }
+        return insert(branch, { key: leafKey, value }) as Leaf<K, V>;
+    }
+
+    let currentBranch: Tree<K, V> | Branch<K, V> = tree;
+    outer: for (let i = 0; rootPath[i]; i++) {
+        const searchKey = rootPath[i];
+
+        const foundBranch: Maybe<TreeNode<K, V>> = find(currentBranch, [
+            searchKey,
+        ]);
+
+        if (!foundBranch) {
+            for (let j = i; rootPath[j]; j++) {
+                const insertKey = rootPath[j];
+                const newBranch: Branch<K, V> = insert(currentBranch, {
+                    key: insertKey,
+                    children: createEmptyTree(),
+                }) as Branch<K, V>;
+
+                currentBranch = newBranch;
+            }
+            break outer;
+        } else if (isLeaf(foundBranch)) {
+            throw new TypeError(
+                "Cannot insert branch because intermediate node is a leaf",
+            );
+        } else {
+            currentBranch = foundBranch;
+        }
+    }
+
+    return insert(currentBranch, {
+        key: leafKey,
+        value,
+    }) as Leaf<K, V>;
+}
+
+export function calculatePath<K, V>(node: TreeNode<K, V>): Path<K> {
+    const path = [node.key];
+    let parentNode = node.parent;
+    while (parentNode) {
+        path.unshift(parentNode.key);
+        parentNode = parentNode.parent;
+    }
+    return path;
+}
+
+function isLeaf<K, V>(node: TreeNode<K, V>): node is Leaf<K, V> {
+    return "value" in (node as Leaf<K, V>);
+}
+
+function isBranch<K, V>(
+    node: TreeNode<K, V> | Tree<K, V>,
+): node is Branch<K, V> {
+    return "children" in (node as Branch<K, V>);
+}
 
 function setParent<K, V>(
     node: PartialTreeNode<K, V>,
-    parent: Branch<K, V> | Symbol,
+    parent: Branch<K, V> | null,
 ): TreeNode<K, V> {
     node.parent = parent;
-    return node as TreeNode<K, V>;
+    if (isBranch(node as TreeNode<K, V>)) {
+        return node as Branch<K, V>;
+    }
+    return node as Leaf<K, V>;
 }
