c1f9dcffbe51bb0e4b328b716c260b8fdb411a00
Brian J Brennan <brianloveswords@gmail.com>
2017-09-16 04:56:31 -0400

    Still need to implement mode filtering but good stopping point.

--- src/command.ts
+++
@@ -1,8 +0,0 @@
-export class Command {
-    public keys: string[];
-    public action: VoidFunction;
-    constructor(cmdstring: string, action: VoidFunction) {
-        this.keys = cmdstring.trim().split(/\s+/);
-        this.action = action;
-    }
-}
