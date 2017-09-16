3fbcf4dfe5c71a721207d3c354d791098cd09285
Brian J Brennan <brianloveswords@gmail.com>
2017-09-16 14:00:51 -0400

    Implements #deepInsertLeaf

--- __tests__/tree.test.ts
+++ __tests__/tree.test.ts
@@ -8,11 +8,41 @@ describe("Tree", () => {
         expect(byeBranch.parent).toBe(hiBranch);
     });
 
-    it("#find: find a node from the tree", () => {
+    it("#insertBranch: sets parents correctly", () => {
+        const t = new Tree<string, string>();
+        const a = t.insertBranch("a");
+        const b = a.insertBranch("b");
+        const c = b.insertBranch("c");
+        const d = c.insertBranch("d");
+        expect(t.parent).toBeUndefined();
+        expect(a.parent).toBe(t);
+        expect(b.parent).toBe(a);
+        expect(c.parent).toBe(b);
+        expect(d.parent).toBe(c);
+    });
+
+    it("#find: finds a node from the tree", () => {
         const t = new Tree<string, string>();
         t.insertBranch("a").insertBranch("b").insertBranch("c");
-        expect(t.find(["a", "b", "c"])).not.toBeNull();
-        expect(t.find(["a", "c"])).toBeNull();
+        expect(t.find(["a", "b", "c"])).toBeDefined();
+        expect(t.find(["a", "c"])).toBeUndefined();
+    });
+
+    it("#insertLeaf: inserts a leaf in the tree", () => {
+        const t = new Tree<string, string>();
+        const expected = t
+            .insertBranch("a")
+            .insertBranch("b")
+            .insertLeaf("c", "the letter c");
+        const found = t.find(["a", "b", "c"]);
+        expect(expected).toBe(found);
+    });
+
+    it("#deepInsertLeaf: inserts a leaf deep into the tree", () => {
+        const t = new Tree<string, string>();
+        const expected = t.deepInsertLeaf(["a", "b", "c"], "the letter c");
+        const found = t.find(["a", "b", "c"]);
+        expect(expected).toBe(found);
     });
 });
 
--- src/tree.ts
+++ src/tree.ts
@@ -1,4 +1,4 @@
-type Maybe<T> = T | null;
+type Maybe<T> = T | undefined;
 
 export interface Branch<K, V> {
     type: "branch";
@@ -20,157 +20,73 @@ export type InternalTree<K, V> = Map<K, TreeNode<K, V>>;
 
 export class Tree<K, V> {
     private internalTree: InternalTree<K, V>;
-    constructor(public parent: Tree<K, V> | null = null) {
+    constructor(public parent?: Tree<K, V>) {
         this.internalTree = new Map();
     }
 
     public insertBranch(name: K): Tree<K, V> {
         const branch: Branch<K, V> = {
             type: "branch",
-            parent: this.parent,
+            parent: this,
             subtree: new Tree(this),
         };
         this.internalTree.set(name, branch);
         return branch.subtree;
     }
 
+    public insertLeaf(name: K, value: V): Leaf<K, V> {
+        const leaf: Leaf<K, V> = {
+            type: "leaf",
+            parent: this,
+            value,
+        };
+        this.internalTree.set(name, leaf);
+        return leaf;
+    }
+
     public find(path: Path<K>): Maybe<TreeNode<K, V>> {
         const key = path.shift();
         const remaining = path;
         if (!key) {
-            return null;
+            return undefined;
         }
         const node = this.internalTree.get(key);
         if (!node) {
-            return null;
+            return undefined;
         }
         if (remaining.length === 0) {
-            return node || null;
+            return node || undefined;
         }
         if (this.isLeaf(node)) {
-            return null;
+            return undefined;
         }
         return node.subtree.find(remaining);
     }
 
-    public isBranch(node: TreeNode<K, V>): node is Branch<K, V> {
-        return node.type === "branch";
+    public deepInsertLeaf(path: Path<K>, value: V): Leaf<K, V> {
+        const key = path.shift();
+        const remaining = path;
+        if (!key) {
+            throw new Error("no key to insert leaf at");
+        }
+        if (remaining.length === 0) {
+            return this.insertLeaf(key, value);
+        }
+
+        const node = this.internalTree.get(key);
+        if (this.isLeaf(node)) {
+            throw new Error("leaf node in the way");
+        }
+        if (!node) {
+            return this.insertBranch(key).deepInsertLeaf(remaining, value);
+        }
+        return node.subtree.deepInsertLeaf(remaining, value);
     }
-    public isLeaf(node: TreeNode<K, V>): node is Leaf<K, V> {
-        return node.type === "leaf";
+
+    public isBranch(node: Maybe<TreeNode<K, V>>): node is Branch<K, V> {
+        return !!(node && node.type === "branch");
+    }
+    public isLeaf(node: Maybe<TreeNode<K, V>>): node is Leaf<K, V> {
+        return !!(node && node.type === "leaf");
     }
 }
-
-// export function findNode<K, V>(
-//     tree: InternalTree<K, V> | Branch<K, V>,
-//     path: Path<K>,
-// ): Maybe<TreeNode<K, V>> {
-//     if (isBranch(tree)) {
-//         tree = tree.children;
-//     }
-
-//     let currentTree = tree;
-//     let count = 0;
-
-//     outer: for (const key of path) {
-//         count += 1;
-
-//         for (const node of currentTree) {
-//             if (node.key === key) {
-//                 if (count === path.length) {
-//                     return node;
-//                 }
-
-//                 if (isLeaf(node)) {
-//                     // node is not a child but we haven't hit the end
-//                     // of the search parameters. technically we
-//                     // haven't found it, so let's return null.
-//                     return null;
-//                 }
-
-//                 currentTree = node.children;
-
-//                 continue outer;
-//             }
-//         }
-//         return null;
-//     }
-//     return null;
-// }
-
-// export function insertLeaf<K, V>(
-//     tree: InternalTree<K, V>,
-//     path: Path<K>,
-//     value: V,
-// ): Leaf<K, V> {
-//     const rootPath = path.slice(0, -1);
-//     const leafKey = path[path.length - 1];
-//     const branch = findNode(tree, rootPath);
-
-//     if (branch) {
-//         if (isLeaf(branch)) {
-//             throw new TypeError("Cannot insert into a leaf node");
-//         }
-//         return insert(branch, { key: leafKey, value }) as Leaf<K, V>;
-//     }
-
-//     let currentBranch: InternalTree<K, V> | Branch<K, V> = tree;
-//     outer: for (let i = 0; rootPath[i]; i++) {
-//         const searchKey = rootPath[i];
-
-//         const foundBranch: Maybe<TreeNode<K, V>> = findNode(currentBranch, [
-//             searchKey,
-//         ]);
-
-//         if (!foundBranch) {
-//             for (let j = i; rootPath[j]; j++) {
-//                 const insertKey = rootPath[j];
-//                 const newBranch: Branch<K, V> = insert(currentBranch, {
-//                     key: insertKey,
-//                     children: createEmptyTree(),
-//                 }) as Branch<K, V>;
-
-//                 currentBranch = newBranch;
-//             }
-//             break outer;
-//         } else if (isLeaf(foundBranch)) {
-//             throw new TypeError(
-//                 "Cannot insert branch because intermediate node is a leaf",
-//             );
-//         } else {
-//             currentBranch = foundBranch;
-//         }
-//     }
-
-//     return insert(currentBranch, {
-//         key: leafKey,
-//         value,
-//     }) as Leaf<K, V>;
-// }
-
-// export function calculatePath<K, V>(node: TreeNode<K, V>): Path<K> {
-//     const path = [node.key];
-//     let parentNode = node.parent;
-//     while (parentNode) {
-//         path.unshift(parentNode.key);
-//         parentNode = parentNode.parent;
-//     }
-//     return path;
-// }
-
-// export function isLeaf<K, V>(node: TreeNode<K, V>): node is Leaf<K, V> {
-//     return "value" in (node as Leaf<K, V>);
-// }
-
-// function setParent<K, V>(
-//     node: PartialTreeNode<K, V>,
-//     parent: Branch<K, V> | null,
-// ): TreeNode<K, V> {
-//     node.parent = parent;
-//     if (isBranch(node as TreeNode<K, V>)) {
-//         return node as Branch<K, V>;
-//     }
-//     return node as Leaf<K, V>;
-// }
-
-// // function filterTree<K, V>(tree: Tree<K, V>, criteria: ()) { }
